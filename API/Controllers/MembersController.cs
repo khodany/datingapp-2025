890e6c4ccb46f652619ae1128d93364670d7ec0f
Khodani Mulisha <khodani.mulisha@dieboldnixdorf.com>
2026-06-09 12:34:29 +0200

    Adding Authetication

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] //localhost:5001/api/
-    [ApiController]
-    public class MembersController(AppDbContext context): ControllerBase
+   
+    public class MembersController(AppDbContext context): BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -18,6 +18,8 @@ namespace API.Controllers
 
          }
 
+
+        [Authorize]
          [HttpGet("{id}")] //localhost:5001/api/bob-id
          public async Task< ActionResult<AppUser>> GetMember(string id)
         {
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,9 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName {get; set; }
     public required string Email {get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+
 
 
 }
